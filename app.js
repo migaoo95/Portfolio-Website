70958bfcca25ab3ccda83a7e5c9374d3bf3c18ae
migaoo95 <45981784+migaoo95@users.noreply.github.com>
2022-02-02 15:30:52 +0000

    Fonts / Image Slider

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// SPLASH TEXT ANIMATION DISPLAY ---------------------------
 // Array with text to display
 const text = [".js", ".php", ".css", ".scss", ".jsx", " .html", ".sql"];
 // Varaibles
@@ -20,3 +21,37 @@ let letter = "";
   }
   setTimeout(type, 600);
 })();
+// SLIDE SHOW ---------------------------
+const slider = document.getElementById("slider");
+const images = ["img1", "img2", "img3"];
+let index = 0;
+document.getElementById("arrow_icon_left").addEventListener("click", () => {
+  if (index > 0) {
+    index--;
+  } else {
+    index = 2;
+  }
+  slider.src = `assets/CryptoStats/${images[index]}.png`;
+  console.log(index);
+  // index--;
+});
+document.getElementById("arrow_icon_right").addEventListener("click", () => {
+  // slider.src = `assets/CryptoStats/${images[index]}.png`;
+  if (index < 2) {
+    index++;
+  } else {
+    index = 0;
+  }
+  slider.src = `assets/CryptoStats/${images[index]}.png`;
+  console.log(index);
+  // index--;
+});
+// document.getElementById("arrow_icon_right").addEventListener("click", () => {
+//   slider.src = `assets/CryptoStats/${images[index]}.png`;
+//   if (index === 2) {
+//     index = 0;
+//   }
+//   index++;
+
+//   console.log(index);
+// });
